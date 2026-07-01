822c0177abc82ca761aadb64a0bab181f8c1d78d
Thomas Phong <ino.thomas@outlook.com>
2026-07-01 11:16:41 +0700

    feat: exercise04

--- src/main/java/com/example/product_management/controller/ProductController.java
+++ src/main/java/com/example/product_management/controller/ProductController.java
@@ -1,13 +1,24 @@
 package com.example.product_management.controller;
 
+import com.example.product_management.model.Product;
 import com.example.product_management.service.IProductService;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
     private final IProductService productService;
 
     public ProductController(IProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
